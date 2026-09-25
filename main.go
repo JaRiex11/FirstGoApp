34634c96728d1f890f640035bbf5580dec5562e3
JaRiex11 <bolotov3sta@gmail.com>
2026-09-25 14:35:32 +0800

    Вынес расчёт дней в отдельную функцию

--- main.go
+++ main.go
@@ -5,21 +5,27 @@ import (
 	"time"
 )
 
-func main() {
-    fmt.Print("Введите дату от которой считать дни (в формате ДД-ММ-ГГГГ): ")
-    timeLayout := "02-01-2006"
-    var input string
-    fmt.Scan(&input)
-    
-    cur_t, err := time.Parse(timeLayout, input) // парсинг значения времени по шаблону
-	if err != nil { panic(err) } // обработка возможной ошибки парсинга
-
+func daysToNextYear(cur_t time.Time) (int){
     nextYearNum := cur_t.Year() + 1
     nextYearDate := time.Date(nextYearNum, time.January, 1, 0, 0, 0, 0, cur_t.Location())
 
     diff := nextYearDate.Sub(cur_t)
 
     daysLeft := int(diff.Hours() / 24)
+    return daysLeft
+}
+
+func main() {
+    fmt.Print("Введите дату от которой считать дни (в формате ДД-ММ-ГГГГ): ")
+    timeLayout := "02-01-2006"
+    var input string
+    fmt.Scan(&input)
+
+    cur_t, err := time.Parse(timeLayout, input) // парсинг значения времени по шаблону
+	if err != nil { panic(err) } // обработка возможной ошибки парсинга
+
+    var daysLeft int
+    daysLeft = daysToNextYear(cur_t)
 
     fmt.Printf("Количество дней до ближайшего Нового Года: %d", daysLeft)
 }
