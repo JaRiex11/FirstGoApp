00ffb57211a829433cf3e5e74e37632b35df84f7
JaRiex11 <bolotov3sta@gmail.com>
2026-09-25 15:06:23 +0800

    Автоматическое форматирование под стандарты Go

--- main.go
+++ main.go
@@ -5,27 +5,29 @@ import (
 	"time"
 )
 
-func daysToNextYear(cur_t time.Time) (int){
-    nextYearNum := cur_t.Year() + 1
-    nextYearDate := time.Date(nextYearNum, time.January, 1, 0, 0, 0, 0, cur_t.Location())
+func daysToNextYear(cur_t time.Time) int {
+	nextYearNum := cur_t.Year() + 1
+	nextYearDate := time.Date(nextYearNum, time.January, 1, 0, 0, 0, 0, cur_t.Location())
 
-    diff := nextYearDate.Sub(cur_t)
+	diff := nextYearDate.Sub(cur_t)
 
-    daysLeft := int(diff.Hours() / 24)
-    return daysLeft
+	daysLeft := int(diff.Hours() / 24)
+	return daysLeft
 }
 
 func main() {
-    fmt.Print("Введите дату от которой считать дни (в формате ДД-ММ-ГГГГ): ")
-    timeLayout := "02-01-2006"
-    var input string
-    fmt.Scan(&input)
+	fmt.Print("Введите дату от которой считать дни (в формате ДД-ММ-ГГГГ): ")
+	timeLayout := "02-01-2006"
+	var input string
+	fmt.Scan(&input)
 
-    cur_t, err := time.Parse(timeLayout, input) // парсинг значения времени по шаблону
-	if err != nil { panic(err) } // обработка возможной ошибки парсинга
+	cur_t, err := time.Parse(timeLayout, input) // парсинг значения времени по шаблону
+	if err != nil {
+		panic(err)
+	} // обработка возможной ошибки парсинга
 
-    var daysLeft int
-    daysLeft = daysToNextYear(cur_t)
+	var daysLeft int
+	daysLeft = daysToNextYear(cur_t)
 
-    fmt.Printf("Количество дней до ближайшего Нового Года: %d", daysLeft)
+	fmt.Printf("Количество дней до ближайшего Нового Года: %d", daysLeft)
 }
